aee61d98b128c0793a37ecc58f5a62208414f153
Moss Aleksandra <amoss@vectradom.vec>
2019-11-28 11:11:32 +0100

    build

---
+++ build/precache-manifest.55db3098c5421b0133b96b011d679df8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3290d25b7978f35e64c9a046f6dbbe3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ceb7d0d3d46191cc55d6",
+    "url": "/static/css/main.ca29437a.chunk.css"
+  },
+  {
+    "revision": "bec91d2380aca79a1f8e",
+    "url": "/static/js/2.17c14689.chunk.js"
+  },
+  {
+    "revision": "ceb7d0d3d46191cc55d6",
+    "url": "/static/js/main.e4bfa6f9.chunk.js"
+  },
+  {
+    "revision": "733894b5161d457cba00",
+    "url": "/static/js/runtime~main.4a4f1216.js"
+  }
+]);
