344dfad71386d4d9f72b5f54aca7cfe752ca40be
Moss Aleksandra <amoss@vectradom.vec>
2019-12-30 20:52:33 +0100

    fixed build folder

---
+++ build/precache-manifest.049dd9f3099921b27315620412c216d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcdb38653fd87fba07e31cde67ccf126",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9b4d2c2f2070f97d1d93",
+    "url": "./static/css/main.ca29437a.chunk.css"
+  },
+  {
+    "revision": "303c12a914ffece6d17a",
+    "url": "./static/js/2.e594a24a.chunk.js"
+  },
+  {
+    "revision": "9b4d2c2f2070f97d1d93",
+    "url": "./static/js/main.c2022134.chunk.js"
+  },
+  {
+    "revision": "f322c1a30355301f70c6",
+    "url": "./static/js/runtime~main.8e7d0fac.js"
+  }
+]);
